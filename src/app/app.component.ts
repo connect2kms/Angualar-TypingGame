ab35af1de9e01afbef205558b3a4eab7e251c03f
Karthik Mohanasundaram <connect2kms@outlook.com>
2021-11-25 00:13:39 -0500

    Received inputed text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'TypingGame';
+  randomText = lorem.sentence();
+  enteredText = '';
+  solved = false;
+
+  onInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.enteredText = value;
+
+    if (value === this.randomText) {
+      this.solved = true;
+    } else {
+      this.solved = false;
+    }
+  }
 }
